ddbc67dbf9049f2d083441c842ba3380fb092246
Booker King <btking1@my.waketech.edu>
2022-06-20 22:36:27 -0400

    code refractor

--- src/page-template.js
+++ src/page-template.js
@@ -1,18 +1,18 @@
 // create the about section
-
 const generateAbout = aboutText => {
   if (!aboutText) {
     return '';
   }
-  
+
   return `
-  <section class="my-3" id="about">
-  <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
-  <p>${aboutText}</p>
-  </section>
+    <section class="my-3" id="about">
+      <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
+      <p>${aboutText}</p>
+    </section>
   `;
 };
 
+// create the projects section
 const generateProjects = projectsArr => {
   return `
     <section class="my-3" id="portfolio">
@@ -26,7 +26,7 @@ const generateProjects = projectsArr => {
             <h3 class="portfolio-item-title text-light">${name}</h3>
             <h5 class="portfolio-languages">
               Built With:
-              ${languages.join(', ')}
+              ${languages.map(language => language).join(',')}
             </h5>
             <p>${description}</p>
             <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
@@ -56,15 +56,14 @@ const generateProjects = projectsArr => {
   `;
 };
 
+// export function to generate entire page
 module.exports = templateData => {
-  
-  
-const { projects, about, ...header } = templateData;
- 
+  // destructure page data by section
+  const { projects, about, ...header } = templateData;
+
   return `
   <!DOCTYPE html>
   <html lang="en">
-
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -74,7 +73,7 @@ const { projects, about, ...header } = templateData;
     <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
   </head>
-
+  
   <body>
     <header>
       <div class="container flex-row justify-space-between align-center py-3">
